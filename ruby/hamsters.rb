4e36ded9e6e1f43444e8cd51a003df6f4640fd28
Brett Buller <brett.buller@gmail.com>
2016-05-24 20:38:00 -0500

    add ruby content to hamsters.rb

---
+++ ruby/hamsters.rb
@@ -0,0 +1,17 @@
+puts "What is the hamster's name?"
+name = gets.chomp
+
+puts "What is the volume level of the hamster from 1 - 10 ?"
+volume = gets.chomp.to_i
+
+puts "What is the hamster's fur color?"
+fur_color = gets.chomp
+
+puts "Is the hamster a good candidate for adoption? (y/n)"
+candidate = gets.chomp
+
+puts "What is the estimated age of the hamster?"
+age = gets.chomp
+if age == ""
+  age = nil
+end
