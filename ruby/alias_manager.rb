2b9860f75e57f4adaaea8b6c4d242c86f98f6e7b
Brett Buller <brett.buller@gmail.com>
2016-06-03 15:21:41 -0500

    add content to make more of a randomized name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,23 +10,35 @@ new_fname = next_name[1]
 
 new_fname2 = new_fname.downcase!
 #new_fname2
-# new_fname2 = "bond"
+# take a name as a string.
+# make it an array and split the two names
+# create a new string with the first name and the last name
+# make them all lower case 
+# change the vowels and the consonants to the next one. save as new string
+# join the two together as new string
+# print the new alias
 
+puts "we need to change your name. What is your first name?"
+
+real_name = gets.chomp
+
+
+next_name = real_name.split (' ')
+# next_name = real_name.split("").shuffle.join
+new_fname = next_name[1]
+# new first name is previous last name
+new_fname2 = new_fname.downcase!
+# new first name is lower case previous last name
 new_lname = next_name[0]
-# new string "James"
+# new last name is previous first name
 new_lname2 = new_lname.downcase!
-# new_lname2 = "james"
-
-new_fname3 = new_fname2.reverse
-# => "dnob"
-
-new_lname3 = new_lname2.reverse
-# => "semaj"
-
-new_lname3 =new_lname3
-
+# new last name is previous first name
+new_fname3 = new_fname2.split("").shuffle.join
+# new first name is randomized
+new_lname3 = new_lname2.split("").shuffle.join
+#new last name is now randomized
 
 new_fakename = new_fname3.capitalize + " " + new_lname3.capitalize
-# new_fakename = "First Last"
+# new alias is randomized first and radomized last name that has been capitalized
 
 puts " Your new alias if you choose to accept it is #{new_fakename}."
