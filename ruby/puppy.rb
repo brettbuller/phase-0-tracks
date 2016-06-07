8dd9f40451fd3536b33e02f4a4607fcfb859aac9
Brett Buller <brett.buller@gmail.com>
2016-06-06 19:13:23 -0500

    create content for puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,22 @@
+###############
+# Puppy Class
+# Name: Chase
+# Breed: Dachshund
+# Color: Red
+# Age: 12 months
+# Weight: 17 pounds
+# Height: Low
+# Bark: Loud
+# Snuggle: Yes
+# Tire: Loves
+
+
+class Puppy
+end
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
+
+greeting = String.new("hello")
+greeting.length
