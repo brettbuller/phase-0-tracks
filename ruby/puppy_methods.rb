602cb0d3af5484e5b7b27a119218382907657571
Brett Buller <brett.buller@gmail.com>
2016-06-07 21:55:55 -0500

    add more code to puppy_methods.rb

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -20,7 +20,11 @@ class Puppy
 	def sit
 		puts "Sitting, now give me a treat."
 	end
-		
+	#intilialize method
+	def initialize
+		puts "Initializing new puppy instance..."
+	end
+
 end
 
 doggy = Puppy.new
@@ -29,3 +33,41 @@ doggy.speak(3)
 doggy.roll_over
 doggy.dog_years(2)
 doggy.sit
+
+class Gymnast
+	#intilialize method
+	def initialize
+		puts "The routine about to begin"
+	end
+	#cool moves 
+	def routine
+		puts "That was an amazing routine, lets see how the Russian Judge scores it."
+	end
+	#scoring from 3 judges and takes average. Returns integer of the average
+	def scoring 
+		score = rand(10)
+		puts "And the score is #{score}."
+	end
+end
+team = []
+
+i = 0
+while  i < 4
+	participant =  "participant#{i}"
+	team << participant
+	i+=1
+end	
+
+team.each do |num| 
+	num = Gymnast.new
+	num.routine
+	num.scoring
+end
+
+
+
+
+
+
+
+
