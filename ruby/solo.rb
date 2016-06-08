199199d83746630be5b7d50d808dbacf7c2e9c05
Brett Buller <brett.buller@gmail.com>
2016-06-08 11:02:07 -0500

    add code to solo.rb

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,8 +1,3 @@
-# new class design
-# class for dachshund
-# attributes = name, length(in inches), color 
-# methods = speak, fetch(toy), chase squirrel 
-
 class Dachshund
 	# location, age, and ethnicity are readable as a getter method
 	attr_reader :name, :length, :color 
@@ -31,6 +26,32 @@ class Dachshund
 	end
 end
 
+# hound = Dachshund.new("Chase", "12", "red")
+# hound.speak
+# hound.fetch("ball")
+# hound.chase_squirrel
+
+hound = []
+dog_kennel = []
+
+# creating the loop 
+puts "How many dachshunds are we rescuing today?"
+checknum = Integer(gets)
+currentnum = 0
 
 
-	
+until checknum == currentnum
+	puts "You have a new Dachshund. What is its name?"
+		d_name = gets.chomp
+		hound << d_name
+	puts "How many inches long is it?"
+		d_inches = gets.chomp.to_i
+		hound << d_inches
+	puts "What color is your long dog?"
+		d_color = gets.chomp
+		hound << d_color
+	checknum -=1
+end
+
+# prints out each dachshund instance
+hound.each {|value| puts "#{value}"}
