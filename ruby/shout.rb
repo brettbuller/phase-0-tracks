4f81cbc11b8cedabf3a2486e7143b5233d27b079
Brett Buller <brett.buller@gmail.com>
2016-06-09 08:10:53 -0500

    add content to shout.rb

---
+++ ruby/shout.rb
@@ -0,0 +1,11 @@
+module Shout
+	def self.yell_angrily(words)
+   		words + "!!!" + " :("
+ 	end
+	def self.yell_happily(words)
+   		words + "!!!" + " :))"
+ 	end
+
+end
+
+Shout.yell_happily("hello")
