32f4a2955c675ba96b3eca30ae41c792eb24cafe
Brett Buller <brett.buller@gmail.com>
2016-06-13 15:23:03 -0500

    create explore.js and add content

---
+++ js/explore.js
@@ -0,0 +1,18 @@
+// start with a string 
+// create a function that takes that string in as an arguement and then returns a reverse of that string
+// the function could separate the string in to letters and then could add them back in the reveres order
+// thinking it could split then reverse then join back.  Hoping it works just like Ruby
+
+function reverse(string) {
+	newString = string.split('').reverse().join('');
+}
+
+reverse("hello")
+console.log(newString)
+
+
+if (newString == "olleh") {
+	console.log("great work");
+}  else {
+	console.log("that is really not the answer");
+
