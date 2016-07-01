6fd8997385ff556c208e70210bcfc9cd73ddf600
Brett Buller <brett.buller@gmail.com>
2016-06-30 20:31:40 -0500

    create campus.erb, new_campuses.erb, student_campus.erb

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,11 +17,40 @@ get '/students/new' do
   erb :new_student
 end
 
+# filter students by campus
+get '/student_campus' do
+  @students_SF = db.execute("SELECT * FROM students WHERE campus='SF'")
+  @students_CHI = db.execute("SELECT * FROM students WHERE campus='CHI'")
+  @students_NYC = db.execute("SELECT * FROM students WHERE campus='NYC'")
+
+erb :student_campus
+end
+
+
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+# create new campus via
+# a from
+
+get '/campuses/new' do
+  erb :new_campuses
+end
+
+post '/campuses' do	
+	db.execute("INSERT INTO campuses (name) VALUES (?)", [params['name']])
+	redirect 'campuses'
+end
+
+get '/campuses' do
+	@campuses = db.execute("SELECT * FROM campuses")
+  erb :campus
+end
+
+
 
 # add static resources
